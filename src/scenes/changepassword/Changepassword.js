c4a99c0d7f0c007e6d8679dad3858b3e0cdca745
Tanucode <dubey.17.tanu@gmail.com>
2023-06-17 09:40:20 +0530

    updated change_password page

--- src/scenes/changepassword/Changepassword.js
+++ src/scenes/changepassword/Changepassword.js
@@ -1,43 +1,45 @@
 import React from 'react';
-import "./changepassword.css";
 import logo512 from "./logo512.png";
+import LockIcon from '@mui/icons-material/LockSharp';
+import {StyledMain,
+        StyledSubmain,
+        StyledCol1,
+        StyledCol2,
+        StyledTitle,
+        StyledGroup1,
+        StyledLockicon,
+        StyledGroup2,
+        StyledGroup3,
+        StyledButton } from "./changepassword.styles";
 
 export default function Changepassword() {
   return (
-    <div className="main">
-        < div className="register">
-        <div className="col-1">
-            <div className="img">
-            <img src={logo512} alt="logo512"/>
-            </div>
+    <StyledMain>
+      <StyledSubmain >
+        <StyledCol1>
+              <img src={logo512} alt="logo512"/>
 
-        </div>
-        <div className="col-2">
-        <span className="title">Change Password</span>
-                <div className="group1">
-                    <span htmlFor="currpassword">
-                     <i class="zmdi zmdi-lock" area-hidde="true"></i>
-                    </span>
+        </StyledCol1>
+
+        <StyledCol2>
+        <StyledTitle>Change Password</StyledTitle>
+                <StyledGroup1>
+                <StyledLockicon> <  LockIcon/> </StyledLockicon>
                     <input type="password" name="currpassword" id="currpassword" autocomplete="off" placeholder="Current Password"/>
-                </div>
-               <div className="group2">
-                  <span htmlFor="newpassword">
-                    <i class="zmdi zmdi-lock"></i>
-                  </span>
+                </StyledGroup1>
+               <StyledGroup2>
+               <StyledLockicon> < LockIcon/> </StyledLockicon>
                   <input type="password" name="newpassword" id="newpassword" autocomplete="off" placeholder="New Password"/>
-              </div>
-              <div className="group3">
-                <span htmlFor="confirmpassword">
-                  <i class="zmdi zmdi-lock"></i>
-                </span>
-                <input type="password" name="confirmpassword" id="confirmpassword" autocomplete="off" placeholder="Confirm Password"/>
-            </div>
-            <div class="container-password-btn">
-                  <button class="password-btn" type="submit">Change Password</button>
-            </div>
-          </div>
-    </div>
+              </StyledGroup2>
+              <StyledGroup3>
+              <StyledLockicon> <  LockIcon/> </StyledLockicon>
+              <input type="password" name="newpassword" id="newpassword" autocomplete="off" placeholder="confirm Password"/>
+            </StyledGroup3>
+                  <StyledButton class="password-btn" type="submit">Change Password</StyledButton>
+          
+          </StyledCol2>
+    </StyledSubmain>
       
-    </div>
+  </StyledMain>
   )
 }
